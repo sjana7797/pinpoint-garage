aa56a78dc8845584015a66c50f825a43f3d111eb
Sanjay Jana <sanjay.jana@valartech.com>
2022-07-16 18:30:03 +0530

    fix: env fix

--- utils/apiAxios.ts
+++ utils/apiAxios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiAxios = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "",
 });
 
 export { apiAxios };
